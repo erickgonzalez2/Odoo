f2391490b0011a05bd933cfd53a83d874d9660b1
erickgonzalez2 <erickamup@gmail.com>
2023-02-11 05:34:02 +0000

    Jala porfa

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+# -* coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Academy',
+    
+    'summary': """Academy app to manage trainning""",
+    
+    'description' : """
+        Academmy Module to manage Trainning
+        -Course
+        -Sessions
+        -Attendees
+    """,
+    
+    'author' : 'Odoo',
+    
+    'website' : 'https://www.odoo.com',
+    
+    'category' : 'Trainning',
+    
+    'version' : '0.1',
+    
+    'depends' : ['base'],
+    
+    'data' : [
+        
+    ],
+    
+    'demo' : [
+        
+    ],
+}
